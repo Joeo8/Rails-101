7d1200f08fe64a092fd89baec370fa87c215f836
Jolly <joe_ob@qq.com>
2021-04-07 15:33:30 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  def index
+    # 配合Bootstrap内建的特效，制作Rails的提示信息
+    flash[:notice] = " 下午好 ！ 您好 ！"
+    # flash[:alert] = " 夜深了 ！ 该睡觉了 ！"
+    # flash[:warning] = "操作不合规范，警告一次！"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # 将 / 根地址指向welcome下的index页面渲染
+  resources :groups
   root 'welcome#index'
 end
