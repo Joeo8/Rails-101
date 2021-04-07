aa5202f2df3364ef321448af4638e5243ea9e7f7
Jolly <joe_ob@qq.com>
2021-04-07 15:37:10 +0800

    replace root with groups#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # 将 / 根地址指向welcome下的index页面渲染
   resources :groups
-  root 'welcome#index'
+  # 将 / 根地址指向welcome下的index页面渲染
+  # root 'welcome#index'
+  # 将跟地址指向讨论组列表页渲染
+  root 'groups#index'
 end
