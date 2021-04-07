8cc07d3a4be6b7ecf24fee37347d9d53038ecf8a
Jolly <joe_ob@qq.com>
2021-04-07 15:18:47 +0800

    add bootstrap flash function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  # 将 / 根地址指向welcome下的index页面渲染
   root 'welcome#index'
 end
