76c670d702ef4a9ab5a6d7544c40b212b1c95e68
Jolly <joe_ob@qq.com>
2021-04-08 09:51:58 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :groups
+  resources :groups do
+    resources :posts
+    
+  end
   # 将 / 根地址指向welcome下的index页面渲染
   # root 'welcome#index'
   # 将跟地址指向讨论组列表页渲染
