0029126306c84830224552104c8b8249c5144c18
Jolly <joe_ob@qq.com>
2021-04-08 09:57:35 +0800

    add post button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups do
     resources :posts
-    
+
   end
   # 将 / 根地址指向welcome下的index页面渲染
   # root 'welcome#index'
